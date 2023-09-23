c8f71fe12fc58459b937911d7981580d4dd4b465
delwerhossain <delwerhossain006@gmail.com>
2023-09-23 12:12:50 +0100

    candied success massage added

--- server-careers/controllers/emailControllers.js
+++ server-careers/controllers/emailControllers.js
@@ -19,8 +19,8 @@ const sendEmail = expressAsyncHandler(async (req, res) => {
 
   // Convert the request body to a string
   // const dataString = JSON.stringify(req.body);
-  const text = `
-  <!DOCTYPE html>
+  const candiedData = `
+    <!DOCTYPE html>
 <html>
 <head>
 <style>
@@ -58,33 +58,117 @@ p {
 </html>
 `;
 
-  let mailOptions = {
-    from: process.env.SMTP_MAIL,
-    to: "hr@anyquotes.co.uk",
-    // to: "delwerhossain006@gmail.com",
-    subject: `Jobs Apply ${jobName}`,
-    text: `AQ`,
-    html: text,
-    attachments: [
-      {
-        filename: `${name}_CV.pdf`,
-        path: `${pdfFile}`,
-        contentType: "application/pdf",
-      },
-    ],
-  };
-
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-      res
-        .status(500)
-        .json({ acknowledged: false, error: "Error sending email" });
-    } else {
-      console.log("Email sent successfully!");
-      res.status(200).json({ acknowledged: true });
+  const successMessage = `
+   <html lang="en">
+      <head>
+        <meta charset="utf-8" />
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title></title>
+        <link
+          href="https://fonts.googleapis.com/css?family=Lato:300,400|Montserrat:700"
+          rel="stylesheet"
+          type="text/css"
+        />
+      <style>
+    body {
+      font-family: sans-serif;
     }
-  });
+
+    h1 {
+      text-align: center;
+    }
+
+    p {
+      margin-bottom: 10px;
+    }
+
+    .quote {
+      font-style: italic;
+      margin-top: 20px;
+    }
+
+    .footer {
+      text-align: right;
+    }
+  </style>
+        <link
+          rel="stylesheet"
+          href="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/default_thank_you.css"
+        />
+        <script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/jquery-1.9.1.min.js"></script>
+        <script src="https://2-22-4-dot-lead-pages.appspot.com/static/lp918/min/html5shiv.js"></script>
+      </head>
+      <body className="mt-16">
+       <h1>Thank You For Apply</h1>
+
+  <p>This is an exciting time in your career, and we are thrilled to have you join the AnyQuotes team. We know you have a lot to offer, and we are eager to see what you can accomplish.</p>
+
+  <p>As you prepare for your new role, we encourage you to take some time to relax and recharge. This is your last chance to enjoy some downtime before you start your new job.</p>
+
+  <p>When you start your new job, please be sure to come prepared to learn and grow. We have a lot of resources available to help you succeed, and we are always happy to answer your questions.</p>
+
+  <p>We are confident that you will be a valuable asset to our team, and we look forward to working with you.</p>
+
+  <p class="quote">"The only way to do great work is to love what you do. If you haven't found it yet, keep looking. Don't settle." - Steve Jobs</p>
+
+  <p class="footer">Best regards,<br>
+  The AnyQuotes Team</p>
+        <footer className="site-footer" id="footer">
+          <p className="site-footer__fineprint" id="fineprint">
+            © 2023 ANY QUOTES - All Right Reserved.
+          </p>
+        </footer>
+      </body>
+    </html>
+    `;
+
+  // Create an array of mail options, one for each recipient
+  const mailOptions = [
+    {
+      // from: "hr@anyquotes.co.uk",
+      to: "delwerhossain006@gmail.com",
+      subject: `Jobs Apply ${jobName}`,
+      text: `AQ`,
+      html: candiedData,
+      attachments: [
+        {
+          filename: `${name}_CV.pdf`,
+          path: `${pdfFile}`,
+          contentType: "application/pdf",
+        },
+      ],
+    },
+    {
+      from: "hr@anyquotes.co.uk",
+      to: email,
+      subject: `successfully applied on anyquotes `,
+      text: `AQ`,
+      html: successMessage,
+    },
+  ];
+
+  try {
+    // Send each email
+    for (const mailOption of mailOptions) {
+      transporter.sendMail(mailOption, function (error, info) {
+        if (error) {
+          console.log(error);
+          res
+            .status(500)
+            .json({ acknowledged: false, error: "Error sending email" });
+          throw error;
+        } else {
+          console.log("Email sent successfully to " + mailOption.to);
+        }
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  } finally {
+    // Respond to the client with success
+    res.status(200).json({ acknowledged: true });
+  }
 });
 
 module.exports = { sendEmail };
