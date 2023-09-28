5ebede06b209e5d697af586c78f12cb97e0b9a43
delwerhossain <delwerhossain006@gmail.com>
2023-09-28 16:02:00 +0100

    menu add for admin

--- client-careers/src/Common/common/Navbar/Navbar.jsx
+++ client-careers/src/Common/common/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import useAuth from "../../../hooks/useAuth";
 import { FiLogIn, FiLogOut } from "react-icons/fi";
 
@@ -21,55 +21,61 @@ const Navbar = () => {
   const menuList = (
     <>
       <ul className=" menu bg-base-200  lg:menu-horizontal rounded-box">
-        {/* <li className="border border-collapse">
-          <NavLink
-            to={"/"}
-            className={`' ' + ${(isActive) => {
-              isActive ? "bg-green-600" : "";
-            }}`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-              />
-            </svg>
-            Home
-            <span className="badge badge-sm">99+</span>
-          </NavLink>
-        </li> */}
-        {/* <li className="border border-collapse ">
-          <NavLink
-            to={"/"}
-            className={`' ' + ${(isActive) => {
-              isActive ? "bg-green-600" : "";
-            }}`}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            Add Data
-          </NavLink>
-        </li>
+        {user && (
+          <>
+            {" "}
+            <li className="border border-collapse">
+              <NavLink
+                to={"/"}
+                className={`' ' + ${(isActive) => {
+                  isActive ? "bg-green-600" : "";
+                }}`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                  />
+                </svg>
+                <span className="font-bold"> Home</span>
+              </NavLink>
+            </li>
+            <li className="border border-collapse ">
+              <NavLink
+                to={"/post"}
+                className={`' ' + ${(isActive) => {
+                  isActive ? "bg-green-600" : "";
+                }}`}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+
+                <span className="font-bold"> Add Post</span>
+              </NavLink>
+            </li>
+          </>
+        )}
+        {/* 
         <li className="border border-collapse">
           <NavLink
             className={(isActive) => ({
