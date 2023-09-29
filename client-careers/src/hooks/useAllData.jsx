ca1c53605c59e4155aa5d79adf06605cb7e5ad22
delwerhossain <delwerhossain006@gmail.com>
2023-09-29 10:56:51 +0100

    all data hook create

---
+++ client-careers/src/hooks/useAllData.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import useAxiosSecure from "./useAxiosSecure";
+
+const useAllData = () => {
+  const [data, setData] = useState([]);
+//   console.log(data);
+  const [axiosSecure] = useAxiosSecure();
+
+  const handleData = () => {
+    axiosSecure
+      .get("/post")
+        .then((data) => {
+           setData(data?.data);
+        })
+      .catch((error) => console.error(error));
+  };
+
+  useEffect(() => {
+    handleData();
+  }, []);
+
+  return data;
+};
+
+export default useAllData;
