1d25de37681f830eef1c240ae0339e1dfc8b524a
delwerhossain <delwerhossain006@gmail.com>
2023-10-10 15:31:50 +0100

    Admin with protect route

--- client-careers/src/routes/PrivateRoute.jsx
+++ client-careers/src/routes/PrivateRoute.jsx
@@ -1,17 +1,31 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router";
 import { AuthContext } from "../provider/AuthProvider";
+import useAdmin from "../hooks/useAdmin";
+import useAuth from "../hooks/useAuth";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
+  const [isAdmin, isAdminLoading] = useAdmin();
+  const {signOutLog} =useAuth()
   const location = useLocation();
 
-  if (loading) {
+  if (loading || isAdminLoading) {
     return <progress className="progress w-56"></progress>;
   }
 
-  if (user) {
+  if (user && isAdmin) {
+    
+
     return children;
+  }
+  else {
+    signOutLog()
+      .then(() => {
+        // Sign-out successful.
+        localStorage.removeItem("access-token");
+      })
+      .catch(() => {});
   }
   return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
